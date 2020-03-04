6d1b89cc186beac7d1263758e9882f61c0dac86d
Alex <akstorg+n@gmail.com>
2020-03-04 18:05:26 +0200

    не разоблся как как открыть файл

--- csv.py
+++ csv.py
@@ -1,6 +1,13 @@
 import csv
 
-with open('firm1.csv') as file:
-    r = csv.readline(file)
-    for raw in r:
-        print(raw)
+# with open('firm1.csv') as file:
+#     r = csv.readline(file)
+#     for raw in r:
+#         print(raw)
+# open('firm1', 'w')
+
+
+file = open('readme.txt','r')
+data = file.read
+print(data)
+file.close()
